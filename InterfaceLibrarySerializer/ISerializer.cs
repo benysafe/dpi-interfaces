4103533a88d3b7ef87ac91f2ca4d1d8542705ee5
Benigno Eduardo Santos Fernández <benignosantos91@gmail.com>
2024-02-16 16:59:01 -0500

    Agregado el metodo dynamicSerialize() en la interfaz de serialización
    Este nuevo método permite serializar para posteriormente publicar a un recipiente indicado desde el procesador y no al recipiente correspondiente indicado en el fichero de configuración funcional

--- InterfaceLibrarySerializer/ISerializer.cs
+++ InterfaceLibrarySerializer/ISerializer.cs
@@ -30,5 +30,16 @@ namespace InterfaceLibrarySerializer
         /// <param name="id">Idenificador del objeto publisher a agregar.</param>
         /// <param name="publisher">Objeto publicador a agregar.</param>
         void addPublisher(string id, IPublisher publisher);
+
+        /// <summary>
+        /// Serializa el mensaje resultante del procesamiento por modulo. Pero en este caso ademas recipe por
+        /// por parametro el recipiente al que se debe publicar ignorando lo espesificado en el fichero de 
+        /// confuguracion funcional, solo es recomendable para serializadores que tienen asociado un unico publicador, 
+        /// y ese publicador debe permitir publicar en el recipiente indicado sin tener que rehacer el proceso de inicialización. 
+        /// </summary>
+        /// <param name="recipent">Recipiente al que se va a publicar el resultado de la serialización.</param>
+        /// <param name="payload">Mensaje resultado del procesamiento por el modulo.</param>
+        /// <param name="priority" value = "["LOW", "NORMAL", "HIGH"]" default= "NORMAL">Nivel de prioridad con que debe ser atendido el mensaje resultante por el resipiente, si no se espesifica se asume "NORMAL"</param>
+        void dynamicSerialize(string recipent, object payload, string priority = "NORMAL");
     }
 }
